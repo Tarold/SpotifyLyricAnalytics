f47b58b1243ed58a73c24685559f5f681394ab54
tarold <ltdecorer@gmail.com>
2023-05-13 20:01:08 +0300

    Remove useless buttons

--- src/components/ArtistViewer.vue
+++ src/components/ArtistViewer.vue
@@ -3,14 +3,6 @@
     <div class="artist-element">
       <img class="artist-img" :src="artist.image" alt="Artist Image" />
       <h3 class="artist-name">{{ artist.name }}</h3>
-      <span>Choose the scale of analytics:</span>
-      <button class="artist-button" :index="index" @click="getSong">
-        One Song</button
-      ><button class="artist-button" :index="index" @click="getAlbum">
-        One Album</button
-      ><button class="artist-button" :index="index" @click="getAll">
-        All compositions
-      </button>
     </div>
     <div>
       <div>
